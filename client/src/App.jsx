97deeea54d8fb81197cd41d20634f58be3899405
yusuf umar faruq <luwaservices@gmail.com>
2023-09-01 10:51:51 -0700

    [FE] signup and resetpassword page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } 
 import Layout from "./components/Layout"
 import Home from "./pages/Home"
 import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import ResetPassword from "./pages/ResetPassword"
+import ForgotPassword from "./pages/ForgotPassword"
 
 
 const App = () => {
@@ -11,6 +14,9 @@ const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
 
 
       </Route>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { RxHamburgerMenu } from 'react-icons/rx'
 import { AiOutlineClose } from 'react-icons/ai'
 
 
-const RoutesWithoutHeader = ["/sign-in", "/sign-up"]
+const RoutesWithoutHeader = ["/sign-in", "/sign-up", "/reset-password", "/forgot-password"]
 
 const Header = () => {
     const [toggle, setToggle] = useState(false)
@@ -44,7 +44,7 @@ const Header = () => {
                                     Sign In
                                 </NavLink>
                             </li>
-                            <li><NavLink className="nav-links">Sign Up</NavLink></li>
+                            <li><NavLink to="/sign-up" className="nav-links">Sign Up</NavLink></li>
                             <li><NavLink className="text-[1rem] font-bold md:font-normal cursor-pointer hover:opacity-[0.6] px-4 py-2 rounded-[.2rem] bg-primary-600 text-primary-500">Post a Job</NavLink></li>
                         </ul>
                     </nav>
